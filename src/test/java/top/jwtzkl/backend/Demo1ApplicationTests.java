0ba3fd2b73db4ba862dfb023dba0fb9786470165
张昆龙 <3309912161@qq.com>
2023-07-04 23:39:25 +0800

    初始化项目

---
+++ src/test/java/top/jwtzkl/backend/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jwtzkl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
