aab2cb48bd0479b5837d61e750d092e976bbc1c7
张昆龙 <3309912161@qq.com>
2023-07-07 13:04:13 +0800

    本地文件存储，oss文件存储

--- src/test/java/top/jwtzkl/backend/AppRunTests.java
+++ src/test/java/top/jwtzkl/backend/AppRunTests.java
@@ -3,11 +3,21 @@ package top.jwtzkl.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 @SpringBootTest
 class AppRunTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testUuid(){
+        for (int i = 0; i < 1000; i++) {
+            String uuid = UUID.randomUUID().toString();
+            System.out.println(uuid);
+        }
     }
 
 }
